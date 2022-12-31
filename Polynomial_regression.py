7686ffc996bff6e93c173431acbf0f0077669378
Vijay <vijayvenkatesh2212@gmail.com>
2022-12-31 21:08:27 +0530

    polynomial R

---
+++ Polynomial_regression.py
@@ -0,0 +1,211 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math, copy
+
+
+def zscore_normalize_features(X, rtn_ms=False):
+    """
+    returns z-score normalized X by column
+    Args:
+      X : (numpy array (m,n)) 
+    Returns
+      X_norm: (numpy array (m,n)) input normalized by column
+    """
+    mu = np.mean(X, axis=0)
+    sigma = np.std(X, axis=0)
+    X_norm = (X - mu) / sigma
+    if rtn_ms:
+        return (
+         X_norm, mu, sigma)
+    return X_norm
+
+def compute_cost(X, y, w, b):
+    """
+    compute cost
+    Args:
+      X : (ndarray): Shape (m,n) matrix of examples with multiple features
+      w : (ndarray): Shape (n)   parameters for prediction   
+      b : (scalar):              parameter  for prediction   
+    Returns
+      cost: (scalar)             cost
+    """
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        f_wb_i = np.dot(X[i], w) + b
+        cost = cost + (f_wb_i - y[i]) ** 2
+
+    cost = cost / (2 * m)
+    return np.squeeze(cost)
+
+def compute_gradient_matrix(X, y, w, b):
+    """
+    Computes the gradient for linear regression 
+ 
+    Args:
+      X : (array_like Shape (m,n)) variable such as house size 
+      y : (array_like Shape (m,1)) actual value 
+      w : (array_like Shape (n,1)) Values of parameters of the model      
+      b : (scalar )                Values of parameter of the model      
+    Returns
+      dj_dw: (array_like Shape (n,1)) The gradient of the cost w.r.t. the parameters w. 
+      dj_db: (scalar)                The gradient of the cost w.r.t. the parameter b. 
+                                  
+    """
+    m, n = X.shape
+    f_wb = X @ w + b
+    e = f_wb - y
+    dj_dw = 1 / m * (X.T @ e)
+    dj_db = 1 / m * np.sum(e)
+    return (
+     dj_db, dj_dw)
+
+def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters):
+    """
+    Performs batch gradient descent to learn theta. Updates theta by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      X : (array_like Shape (m,n)    matrix of examples 
+      y : (array_like Shape (m,))    target value of each example
+      w_in : (array_like Shape (n,)) Initial values of parameters of the model
+      b_in : (scalar)                Initial value of parameter of the model
+      cost_function: function to compute cost
+      gradient_function: function to compute the gradient
+      alpha : (float) Learning rate
+      num_iters : (int) number of iterations to run gradient descent
+    Returns
+      w : (array_like Shape (n,)) Updated values of parameters of the model after
+          running gradient descent
+      b : (scalar)                Updated value of parameter of the model after
+          running gradient descent
+    """
+    m = len(X)
+    hist = {}
+    hist['cost'] = []
+    hist['params'] = []
+    hist['grads'] = []
+    hist['iter'] = []
+    w = copy.deepcopy(w_in)
+    b = b_in
+    save_interval = np.ceil(num_iters / 10000)
+    for i in range(num_iters):
+        dj_db, dj_dw = gradient_function(X, y, w, b)
+        w = w - alpha * dj_dw
+        b = b - alpha * dj_db
+        if not i == 0:
+            if i % save_interval == 0:
+                hist['cost'].append(cost_function(X, y, w, b))
+                hist['params'].append([w, b])
+                hist['grads'].append([dj_dw, dj_db])
+                hist['iter'].append(i)
+            if i % math.ceil(num_iters / 10) == 0:
+                cst = cost_function(X, y, w, b)
+                print(f"Iteration {i:9d}, Cost: {cst:0.5e}")
+
+    return (
+     w, b, hist)
+
+
+def run_gradient_descent_feng(X, y, iterations=1000, alpha=1e-06):
+    m, n = X.shape
+    initial_w = np.zeros(n)
+    initial_b = 0
+    w_out, b_out, hist_out = gradient_descent(X, y, initial_w, initial_b, compute_cost, compute_gradient_matrix, alpha, iterations)
+    print(f"w,b found by gradient descent: w: {w_out}, b: {b_out:0.4f}")
+    return (
+     w_out, b_out)
+
+
+
+
+
+# create target data
+x = np.arange(0, 20, 1)
+y = 1 + x**2
+X = x.reshape(-1, 1)
+
+
+model_w,model_b = run_gradient_descent_feng(X,y,iterations=1000, alpha = 1e-2)
+
+plt.scatter(x, y, marker='x', c='r', label="Actual Value"); plt.title("no feature engineering")
+plt.plot(x,X@model_w + model_b, label="Predicted Value");  plt.xlabel("X"); plt.ylabel("y"); plt.legend(); plt.show()
+
+# create target data
+x = np.arange(0, 20, 1)
+y = 1 + x**2
+
+# Engineer features 
+X = x**2      #<-- added engineered feature
+
+X = X.reshape(-1, 1)  #X should be a 2-D Matrix
+model_w,model_b = run_gradient_descent_feng(X, y, iterations=10000, alpha = 1e-5)
+
+plt.scatter(x, y, marker='x', c='r', label="Actual Value"); plt.title("Added x**2 feature")
+plt.plot(x, np.dot(X,model_w) + model_b, label="Predicted Value"); plt.xlabel("x"); plt.ylabel("y"); plt.legend(); plt.show()
+
+
+# Selecting Features
+# create target data
+x = np.arange(0, 20, 1)
+y = x**2
+
+# engineer features .
+X = np.c_[x, x**2, x**3]   #<-- added engineered feature
+
+model_w,model_b = run_gradient_descent_feng(X, y, iterations=10000, alpha=1e-7)
+
+plt.scatter(x, y, marker='x', c='r', label="Actual Value"); plt.title("x, x**2, x**3 features")
+plt.plot(x, X@model_w + model_b, label="Predicted Value"); plt.xlabel("x"); plt.ylabel("y"); plt.legend(); plt.show()
+
+# An alternate view
+# create target data
+x = np.arange(0, 20, 1)
+y = x**2
+
+# engineer features .
+X = np.c_[x, x**2, x**3]   #<-- added engineered feature
+X_features = ['x','x^2','x^3']
+
+
+fig,ax=plt.subplots(1, 3, figsize=(12, 3), sharey=True)
+for i in range(len(ax)):
+    ax[i].scatter(X[:,i],y)
+    ax[i].set_xlabel(X_features[i])
+ax[0].set_ylabel("y")
+plt.show()
+
+# Scaling features
+# create target data
+x = np.arange(0,20,1)
+X = np.c_[x, x**2, x**3]
+print(f"Peak to Peak range by column in Raw        X:{np.ptp(X,axis=0)}")
+
+# add mean_normalization 
+X = zscore_normalize_features(X)     
+print(f"Peak to Peak range by column in Normalized X:{np.ptp(X,axis=0)}")
+
+
+# Change alpha
+x = np.arange(0,20,1)
+y = x**2
+
+X = np.c_[x, x**2, x**3]
+X = zscore_normalize_features(X) 
+
+model_w, model_b = run_gradient_descent_feng(X, y, iterations=100000, alpha=1e-1)
+
+plt.scatter(x, y, marker='x', c='r', label="Actual Value"); plt.title("Normalized x x**2, x**3 feature")
+plt.plot(x,X@model_w + model_b, label="Predicted Value"); plt.xlabel("x"); plt.ylabel("y"); plt.legend(); plt.show()
+
+# Complex Functions
+x = np.arange(0,20,1)
+y = np.cos(x/2)
+
+X = np.c_[x, x**2, x**3,x**4, x**5, x**6, x**7, x**8, x**9, x**10, x**11, x**12, x**13]
+X = zscore_normalize_features(X) 
+
+model_w,model_b = run_gradient_descent_feng(X, y, iterations=1000000, alpha = 1e-1)
+
+plt.scatter(x, y, marker='x', c='r', label="Actual Value"); plt.title("Normalized x x**2, x**3 feature")
+plt.plot(x,X@model_w + model_b, label="Predicted Value"); plt.xlabel("x"); plt.ylabel("y"); plt.legend(); plt.show()
